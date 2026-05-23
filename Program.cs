02fc8810390205bda8594d7ae8d1c874e02787cd
Gulshan <gulsh@ADMIN>
2026-05-23 15:12:15 +0530

    Changr

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@ using JobPortalCORE.Models;
 using JobPortalCORE.Data;
 using JobPortalCORE.Filters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection; // (Ye agar pehle se hai toh theek)
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// 1. Database & Identity Services
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+// 2. Controllers & Pages (Dono ko mila diya)
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews(options =>
 {
@@ -17,15 +21,17 @@ builder.Services.AddControllersWithViews(options =>
     options.Filters.Add<GlobalExceptionFilter>();
 });
 
+// 3. Application Insights (BILKUL SAHI JAGAH PAR HAI!)
 builder.Services.AddApplicationInsightsTelemetry();
 
+// ----------------------------------------------------
 var app = builder.Build();
+// ----------------------------------------------------
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -35,6 +41,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
